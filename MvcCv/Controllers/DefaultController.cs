8caa79a6d616863c09ae0d277a3892694bb65b25
habibesayrac <habibesayrac@outlook.com>
2024-02-13 11:55:07 +0300

    Add project files.

---
+++ MvcCv/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCv.Models.Entity;
+
+namespace MvcCv.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        DbCvEntities db = new DbCvEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLHakkimda.ToList();
+
+            return View(degerler);
+        }
+    }
+}
