6de6d9f3b7f8cbb7cad706e66b4c57752da83091
habibesayrac <habibesayrac@outlook.com>
2024-02-15 21:12:11 +0300

    yetenekekle added

---
+++ MvcCv/Controllers/YetenekController.cs
@@ -0,0 +1,33 @@
+﻿using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCv.Controllers
+{
+    public class YetenekController : Controller
+    {
+        YetenekRepository repo = new YetenekRepository();
+        public ActionResult Index()
+        {
+            var yetenek = repo.List();
+            return View(yetenek);
+        }
+        [HttpGet]
+        public ActionResult YetenekEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YetenekEkle(TBLYeteneklerim p)
+        {
+            repo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
