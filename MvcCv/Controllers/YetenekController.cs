fdf8406428734388c1bc1902880d9a666dd794e5
habibesayrac <habibesayrac@outlook.com>
2024-02-15 23:19:14 +0300

    yeteneklerim area completed

--- MvcCv/Controllers/YetenekController.cs
+++ MvcCv/Controllers/YetenekController.cs
@@ -27,7 +27,27 @@ namespace MvcCv.Controllers
             repo.TAdd(p);
             return RedirectToAction("Index");
         }
+        public ActionResult YetenekSil(int id)
+        {
+            TBLYeteneklerim t = repo.Find(x => x.ID == id);
+            repo.TDelete(t);
+            return RedirectToAction("Index");
 
-
+        }
+        [HttpGet]
+        public ActionResult YetenekGetir(int id)
+        {
+           var yetenek = repo.Find(x=>x.ID == id);
+            return View(yetenek);
+        } 
+        [HttpPost]
+        public ActionResult YetenekGetir(TBLYeteneklerim p)
+        {
+           var yetenek = repo.Find(x=>x.ID == p.ID);
+            yetenek.Yetenek = p.Yetenek;
+            yetenek.Oran = p.Oran;
+            repo.TUpdate(yetenek);
+            return RedirectToAction("Index");
+        }
     }
 }
