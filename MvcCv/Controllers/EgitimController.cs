29dca5beada08db04781073262b3b79e08e16fe6
habibesayrac <habibesayrac@outlook.com>
2024-02-16 10:00:47 +0300

    egitimlerim area completed

---
+++ MvcCv/Controllers/EgitimController.cs
@@ -0,0 +1,63 @@
+﻿using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCv.Controllers
+{
+    public class EgitimController : Controller
+    {
+        EgitimRepository repo = new EgitimRepository();
+        public ActionResult Index()
+        {
+            var egitim = repo.List();
+            return View(egitim);
+        }
+        [HttpGet]
+        public ActionResult EgitimEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult EgitimEkle(TBLEgitimlerim p)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("EgitimEkle");
+            }
+            repo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+        public ActionResult EgitimSil(int id)
+        {
+            var egitim = repo.Find(x => x.ID == id);
+            repo.TDelete(egitim);
+            return RedirectToAction("Index");
+
+        }
+        [HttpGet]
+        public ActionResult EgitimGetir(int id)
+        {
+            var egitim = repo.Find(x => x.ID == id);
+
+            return View(egitim);
+        }
+        [HttpPost]
+        public ActionResult EgitimGetir(TBLEgitimlerim p)
+        {
+            var egitim = repo.Find(x => x.ID == p.ID);
+            egitim.Baslik = p.Baslik;
+            egitim.AltBaslik1 = p.AltBaslik1;
+            egitim.AltBaslik2 = p.AltBaslik2;
+            egitim.GNO = p.GNO;
+            egitim.Tarih = p.Tarih;
+            repo.TUpdate(egitim);
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
