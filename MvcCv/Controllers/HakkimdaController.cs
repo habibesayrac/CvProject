8204e516837d8b456ec8e9d1cb706ad4d3d9bb4d
habibesayrac <habibesayrac@outlook.com>
2024-02-18 21:22:01 +0300

    hakkimda area completed

--- MvcCv/Controllers/HakkimdaController.cs
+++ MvcCv/Controllers/HakkimdaController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,28 @@ namespace MvcCv.Controllers
 {
     public class HakkimdaController : Controller
     {
-        // GET: Hakkimda
+
+        HakkimdaRepository repo = new HakkimdaRepository();
+
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            var hakkimda = repo.List();
+            return View(hakkimda);
+        }
+        [HttpPost]
+        public ActionResult Index(TBLHakkimda p)
+        {
+            var hakkimda = repo.Find(x=>x.ID == p.ID);
+            hakkimda.Ad = p.Ad;
+            hakkimda.Soyad= p.Soyad;
+            hakkimda.Adres = p.Adres;
+            hakkimda.Telefon = p.Telefon;
+            hakkimda.Mail = p.Mail;
+            hakkimda.Aciklama = p.Aciklama;
+            hakkimda.Resim = p.Resim;
+            repo.TUpdate(hakkimda);
+            return RedirectToAction("Index");
         }
     }
 }
