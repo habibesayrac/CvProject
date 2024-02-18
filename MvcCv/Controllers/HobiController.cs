476eafae89bf4010c6b0003c1eef44d995cd7aad
habibesayrac <habibesayrac@outlook.com>
2024-02-18 20:37:39 +0300

    iletisim and hobi area completed

---
+++ MvcCv/Controllers/HobiController.cs
@@ -0,0 +1,35 @@
+﻿using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCv.Controllers
+{
+    public class HobiController : Controller
+    {
+        HobiRepository repo = new HobiRepository();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var hobi = repo.List();
+            return View(hobi);
+        }
+
+        [HttpPost]
+        public ActionResult Index(TBLHobilerim p)
+        {
+
+            var hobi = repo.Find(x => x.ID == 1);
+            hobi.Aciklama1 = p.Aciklama1;
+            hobi.Aciklama2 = p.Aciklama2;
+            repo.TUpdate(hobi);
+            return RedirectToAction("Index");
+
+
+        }
+    }
+}
---
+++ MvcCv/Controllers/IletisimController.cs
@@ -0,0 +1,19 @@
+﻿using MvcCv.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCv.Controllers
+{
+    public class IletisimController : Controller
+    {
+        IletisimRepository repo = new IletisimRepository();
+        public ActionResult Index()
+        {
+            var iletisim = repo.List();
+            return View(iletisim);
+        }
+    }
+}
