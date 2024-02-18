0f65222994746d504c067a14d719e068fb9a0216
habibesayrac <habibesayrac@outlook.com>
2024-02-18 13:37:39 +0300

    sertifikalarım area completed

--- MvcCv/Controllers/SertifikaController.cs
+++ MvcCv/Controllers/SertifikaController.cs
@@ -1,4 +1,5 @@
-﻿using MvcCv.Repositories;
+﻿using MvcCv.Models.Entity;
+using MvcCv.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +15,42 @@ namespace MvcCv.Controllers
         {
             var sertifika = repo.List();
             return View(sertifika);
+        }
+        [HttpGet]
+        public ActionResult SertifikaEkle()
+        {
+         return View();
+        }
+        [HttpPost]
+        public ActionResult SertifikaEkle(TBLSertifikalarim p)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Sertifika Ekle");
+            }
+            repo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+        public ActionResult SertifikaSil(int id)
+        {
+            var sertifika = repo.Find(x=>x.ID== id);
+            repo.TDelete(sertifika);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult SertifikaGetir(int id)
+        {
+            var sertifika = repo.Find(x=>x.ID== id);       
+            return View(sertifika);
+        }
+        [HttpPost]
+        public ActionResult SertifikaGetir(TBLSertifikalarim p)
+        {
+            var sertifika = repo.Find(x=>x.ID== p.ID);
+            sertifika.Tarih = p.Tarih;
+            sertifika.Aciklama = p.Aciklama;
+            repo.TUpdate(sertifika);       
+            return RedirectToAction("Index");
         }
     }
 }
